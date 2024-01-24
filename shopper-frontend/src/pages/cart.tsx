9c099e6843b5256b7498354849e438f5c889e86b
lifehackerr <omkeshvarshney999@gmail.com>
2024-01-25 02:33:32 +0530

    Cart and home page created and modified

--- shopper-frontend/src/pages/cart.tsx
+++ shopper-frontend/src/pages/cart.tsx
@@ -1,8 +1,57 @@
+import { useEffect, useState } from "react";
+import { VscError } from "react-icons/vsc";
+import CartItem from "../Components/cart-item";
+import { Link } from "react-router-dom";
+const cartItems = [
+  {
+    productId:"jhbd",
+    name:"Macbook",
+    price:3000,
+    stock:40,
+    quantity:4,
+    photo : "https://m.media-amazon.com/images/I/71eXNIDUGjL._SL1500_.jpg"
+  }
+];
+const subTotal = 4000;
+const tax = Math.round(subTotal * 0.18);
+const shippingCharges = 200;
+const discount = 400;
+const total = subTotal + tax + shippingCharges -discount;
 
 const Cart = () => {
+  const [couponCode, setCouponCode] = useState("");
+  const [isValidCouponCode, setIsValidCouponCode] = useState(false);
+  useEffect(() => { 
+    const timeOutId = setTimeout(() =>{
+      if(Math.random()>0.5) setIsValidCouponCode(true);
+      else setIsValidCouponCode(false);
+    },100);
+    return () =>{
+      clearTimeout(timeOutId);
+      setIsValidCouponCode(false);
+    }},[couponCode])
   return (
-    <div>
-      Cart
+    <div className="cart">
+      <main>
+          {cartItems.length > 0 ? cartItems.map((i,idx)=> (<CartItem key = {idx} cartItem={i} />)
+          ): <h1>No Items Added</h1>}
+      </main>
+      <aside>
+          <p> Subtotal: ${subTotal}</p>
+          <p> Shipping Charges: ${shippingCharges}</p>
+          <p> tax: ${tax}</p>
+          <p> Discount: <em className="red">${discount}</em></p>
+          <p> Total: <b>${total}</b></p>
+          <input type="text" placeholder="coupon code" value={couponCode} onChange={(e)=>setCouponCode(e.target.value)}></input>
+        {
+          couponCode && isValidCouponCode ? <span className="green"> ${discount} off using this <code> {couponCode}</code></span>:
+          <span className="red">Invalid Coupon <VscError/></span>
+        }
+        {
+          cartItems.length > 0 &&  <Link to= "/shipping"> Checkout</Link>
+        }
+
+      </aside>
     </div>
   )
 }
--- shopper-frontend/src/pages/home.tsx
+++ shopper-frontend/src/pages/home.tsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom"
+import ProductCard from "../Components/product-card"
 const Home = () => {
+  const addToCartHandler = () =>{};
   return (
     <div className="home">
       <section></section>
@@ -7,6 +9,9 @@ const Home = () => {
         Latest Products
         <Link to = "/search" className= "findmore">More</Link>
       </h1>
+      <main>
+        <ProductCard productId="jhbd" name="macbook" price={4500} stock={3} handler={addToCartHandler} photo="https://m.media-amazon.com/images/I/71eXNIDUGjL._SL1500_.jpg" />
+      </main>
     </div>
   )
 }
